4052859e66888d9093aab9547fb4b66d29ce3180
Yash Singh <saiansh2525@gmail.com>
2022-01-26 02:35:38 +0000

    feat: Added more commands and removed version command

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ async def on_ready():
   print('Logged on as', client.user.name)
 
 async def helpCommand(ctx):
-  embed = discord.Embed(description="Help on using the Mermaid.js Discord Bot", color=4372867)
+  embed = discord.Embed(description="Help on using the Mermaid.js Discord Bot.", color=4372867)
   embed.set_author(name="Help")
-  embed.add_field(name="Helpful Links", value="[GitHub](https://github.com/Yash-Singh1/mermaidjs-discord) | [Issue Tracker](https://github.com/Yash-Singh1/mermaidjs-discord/issues)", inline=False)
-  embed.add_field(name="Commands", value="__!mermaid-version__: Display the current version of the `mermaidjs-discord`\n__!mermaid-help__: Displays the current message\n__!mermaid-r__ or __!mermaid-render__: Renders the diagram with the code following the command", inline=False)
+  embed.add_field(name="Helpful Links", value="[GitHub](https://github.com/Yash-Singh1/mermaidjs-discord) | [Issue Tracker](https://github.com/Yash-Singh1/mermaidjs-discord/issues) | [Mermaid.js Documentation](https://mermaid-js.github.io/)", inline=False)
+  embed.add_field(name="Commands", value="__!mermaid-help__: Displays the current message.\n__!mermaid-r__ or __!mermaid-render__: Renders the diagram with the code following the command. Allows Mermaid.js diagram syntax.\n__!mermaid-invite__: Gets the invite link of the bot.\n__!mermaid-support__: Gets a link to a support issue tracker for the bot.", inline=False)
   await ctx.send(embed=embed)
 
 @client.event
@@ -24,15 +24,19 @@ async def on_guild_join(guild):
     await helpCommand(guild.system_channel)
 
 @client.command()
-async def version(ctx):
-  await ctx.send('You currently have `mermaidjs-discord` version 0.0.1 installed')
-
-@client.command()
 async def help(ctx):
   await helpCommand(ctx)
 
 @client.command(aliases=['r'])
 async def render(ctx, *, arg):
   await ctx.message.reply('https://mermaid.ink/img/' + base64.b64encode(arg.encode('ascii')).decode('ascii'))
+
+@client.command()
+async def invite(ctx):
+  await ctx.send(embed=discord.Embed(description="The invite link is https://discord.com/api/oauth2/authorize?client_id=935684419837132910&permissions=274877910016&scope=bot", color=4372867))
+
+@client.command()
+async def support(ctx):
+  await ctx.send(embed=discord.Embed(description="The link to the issue tracker is https://github.com/Yash-Singh1/mermaidjs-discord/issues", color=4372867))
 
 client.run(os.environ['TOKEN'])
